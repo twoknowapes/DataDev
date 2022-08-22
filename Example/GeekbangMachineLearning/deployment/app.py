9d36f4088033dac71c37bb0b662c89e75ca4e22b
makey.yang <1007845131@qq.com>
2022-08-23 05:31:30 +0800

    Deployment

---
+++ Example/GeekbangMachineLearning/deployment/app.py
@@ -0,0 +1,27 @@
+import pickle
+
+import numpy as np
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+model = pickle.load(open("model.pkl", "rb"))
+
+
+@app.route("/")
+def home():  # 默认启动页面
+    return render_template("index.html")  # 启动 index.html
+
+
+@app.route("/predict", methods=["POST"])
+def predict():  # 启动预测页面
+    features = [int(x) for x in request.form.values()]  # 输入特征
+    label = [np.array(features)]  # 标签
+    prediction = model.predict(label)  # 预测结果
+
+    output = round(prediction[0], 2)  # 输出预测结果
+
+    return render_template("index.html", prediction_text="浏览量 {}".format(int(output)))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ Example/GeekbangMachineLearning/deployment/model.py
@@ -0,0 +1,23 @@
+import pickle
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+df_ads = pd.read_csv("../ysxhwxrw.csv")
+
+df_ads["转发数"].fillna(df_ads["点赞数"], inplace=True)
+
+X = df_ads.drop(["浏览量"], axis=1)  # 特征集、Drop 掉标签相关字段
+y = df_ads.浏览量  # 标签集
+
+
+regressor = LinearRegression()  # 拟合模型
+
+regressor.fit(X, y)
+
+pickle.dump(regressor, open("model.pkl", "wb"))  # 序列化模型（存盘）
+
+model = pickle.load(open("model.pkl", "rb"))  # 反序列化模型（再导入模型）
+print(model)
+
+# print(model.predict([[300, 800]]))  # 进行一个预测
